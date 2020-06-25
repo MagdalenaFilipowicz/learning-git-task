8803eb91cf17b1337eca51d001b119eddaefe1e0
MagdalenaFilipowicz <67333515+MagdalenaFilipowicz@users.noreply.github.com>
2020-06-25 20:26:15 +0200

    Change shop

--- lista_zakupow2.py
+++ lista_zakupow2.py
@@ -1,5 +1,5 @@
 print("Zadanie 1.1")
-shop = {'piekarnia':['Chleb','pączek','Bułki'] , 'Warzywniak':['Marchew','Seler','Rukola']}
+shop = {'Zoologiczny':['Piesek','Kotek','Rybki'] , 'Warzywniak':['Marchew','Seler','Rukola']}
 for k in shop:
     print(f"Idę do {k.capitalize()}, Kupuję tu następujące rzeczy: {str(shop[k]).title()}")
 print(f"W sumie kupuję {sum(len(v)for v in shop.values())} produktów.")
